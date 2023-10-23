c1882ed03c3ffab6a1be260d7379bea3fd964fe1
LeoTortega <leonardo.tortega@outlook.com>
2023-10-23 20:50:50 -0300

    Primeiro commit

---
+++ src/main/java/com/clicktech/clicktech/ClickTechApplication.java
@@ -0,0 +1,13 @@
+package com.clicktech.clicktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clicktech/clicktech/ClickTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clicktech.clicktech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
